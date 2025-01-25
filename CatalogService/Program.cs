a21a2ea96359d705e4a8a773e87cb3be7ea88885
dgroumpos <dngroumpos@hotmail.com>
2025-01-25 12:29:03 +0200

    implemented repository pattern

--- CatalogService/Program.cs
+++ CatalogService/Program.cs
@@ -1,4 +1,5 @@
 using CatalogService.Data;
+using CatalogService.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<CatalogDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
